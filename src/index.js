b3ef80abe23d05ce181349b0634bb35ba1282c81
unknown <poputa.alex@gmail.com>
2024-09-27 13:41:57 +0300

    changed title

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import { createCards, modalDisplay, gallery, data } from './scripts/data';
 
 const modal = document.querySelector('.modal__container'),
-    closeBtn = document.querySelector('.modal__close-btn');
-    
+  closeBtn = document.querySelector('.modal__close-btn');
 
 createCards();
 
 gallery.addEventListener('click', e => {
-    if (e.target.nodeName !== 'IMG') {
-        return;
-    }
-    const selectedImg = e.target.src;
-    modal.classList.toggle('hidden');
-    modalDisplay(selectedImg);
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(e.target.nodeName);
+  const selectedImg = e.target.src;
+  modal.classList.toggle('hidden');
+  modalDisplay(selectedImg);
 });
 
 closeBtn.addEventListener('click', () => {
-    modal.classList.toggle('hidden');
+  modal.classList.toggle('hidden');
 });
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -4,31 +4,31 @@ import data from '../../assets/data/data.json';
 const gallery = document.querySelector('#display__canvas');
 
 const createCards = () => {
-    data.forEach(({ imgUrl }) => {
-        const card = `<div class="card">
+  data.forEach(({ imgUrl }) => {
+    const card = `<div class="card">
                         <img src="${imgUrl}" class="display__img" loading="lazy">
                       </div>`;
 
-        gallery.insertAdjacentHTML('beforeend', card);
-    });
+    gallery.insertAdjacentHTML('beforeend', card);
+  });
 };
 
 // display correct data in modal
 const modalImg = document.querySelector('.modal__img'),
-    title = document.querySelector('.modal__title'),
-    modalDescr = document.querySelector('.modal__descr'),
-    modalLink = document.querySelector('.modal__link'),
-    modalTech = document.querySelector('.modal__tech');
+  title = document.querySelector('.modal__title'),
+  modalDescr = document.querySelector('.modal__descr'),
+  modalLink = document.querySelector('.modal__link'),
+  modalTech = document.querySelector('.modal__tech');
 
-const modalDisplay = (selectedImg) => {
-    const myItem = data.filter(item => item.imgUrl === selectedImg);
-    const [{ name, descr, imgUrl, tech, link, id }] = myItem;
-    modalImg.src = imgUrl;
-    title.textContent = name;
-    modalDescr.textContent = descr;
-    modalLink.href = link;
-    modalLink.textContent = name;
-    modalTech.textContent = tech;
-}
+const modalDisplay = selectedImg => {
+  const myItem = data.filter(item => item.imgUrl === selectedImg);
+  const [{ name, descr, imgUrl, tech, link }] = myItem;
+  modalImg.src = imgUrl;
+  title.textContent = name;
+  modalDescr.textContent = descr;
+  modalLink.href = link;
+  modalLink.textContent = name;
+  modalTech.textContent = tech;
+};
 
 export { data, gallery, createCards, modalDisplay };
